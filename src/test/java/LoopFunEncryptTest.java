4e456cc7a101b8902c453c32158af31305deda32
vle <obentoka@gmail.com>
2020-02-15 10:31:47 -0500

    initial quiz blank

---
+++ src/test/java/LoopFunEncryptTest.java
@@ -0,0 +1,43 @@
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LoopFunEncryptTest
+{
+    private LoopFun loop;
+
+    @Before
+    public void setUp(){
+        loop = new LoopFun();
+    }
+
+
+
+    @Test
+    public void testEncryptWithTheFirstAlphabet(){
+        //Given
+        String word = "apple";
+        String expected = "dssoh";
+
+        //When
+        String actual = loop.encrypt(word);
+
+        //Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEncryptWithTheLastLetters() {
+        //Given
+        String word = "wxyz";
+        String expected = "zabc";
+
+        //When
+        String actual = loop.encrypt(word);
+
+        //Then
+        assertEquals(expected, actual);
+    }
+}
